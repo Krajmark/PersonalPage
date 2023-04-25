28404641a0f4994ba88eebed92d9f5662d380f74
Krajnyák Márk <kmark.mrkk@gmail.com>
2023-04-25 18:39:09 +0200

    init: basic springboot setup

---
+++ src/test/java/com/krajmark/personalpage/PersonalPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krajmark.personalpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
